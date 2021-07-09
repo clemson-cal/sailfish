1d5a161841e41217d85487d998ba88b9ee855bf8
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-07-09 13:04:09 -0400

    Attempt to generalize gpu_mem to ROCm

--- gpu_mem/build.rs
+++ gpu_mem/build.rs
@@ -1,7 +1,34 @@
+use std::env;
+use std::fs;
+
+fn is_program_in_path(program: &str) -> bool {
+    if let Ok(path) = env::var("PATH") {
+        for p in path.split(":") {
+            let p_str = format!("{}/{}", p, program);
+            if fs::metadata(p_str).is_ok() {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn main() {
-    cc::Build::new()
-        .file("src/lib.cu")
-        .cuda(true)
-        .compile("lib");
-    println!("cargo:rustc-link-lib=dylib=cudart");
+    if is_program_in_path("nvcc") {
+        cc::Build::new()
+            .file("src/lib.cu")
+            .cuda(true)
+            .flag("-x=cu")
+            .compile("lib");
+        println!("cargo:rustc-link-lib=dylib=cudart");
+    } else if is_program_in_path("hipcc") {
+        cc::Build::new()
+            .file("src/lib.cu")
+            .compiler("hipcc")
+            .compile("lib");
+        println!("cargo:rustc-link-search=native=/opt/rocm/lib");
+        println!("cargo:rustc-link-lib=dylib=amdhip64");
+    } else {
+        panic!("nvcc is not installed");
+    }
 }
--- gpu_mem/src/lib.cu
+++ gpu_mem/src/lib.cu
@@ -1,32 +1,52 @@
 #include <math.h>
 
+#ifdef __NVCC__
+#define EXTERN_C extern "C"
+#define gpuFree cudaFree
+#define gpuMalloc cudaMalloc
+#define gpuMemcpy cudaMemcpy
+#define gpuMemcpyHostToDevice cudaMemcpyHostToDevice
+#define gpuMemcpyDeviceToHost cudaMemcpyDeviceToHost
+#define gpuMemcpyDeviceToDevice cudaMemcpyDeviceToDevice
+
+#else
+#include <hip/hip_runtime.h>
+#define EXTERN_C
+#define gpuFree hipFree
+#define gpuMalloc hipMalloc
+#define gpuMemcpy hipMemcpy
+#define gpuMemcpyHostToDevice hipMemcpyHostToDevice
+#define gpuMemcpyDeviceToHost hipMemcpyDeviceToHost
+#define gpuMemcpyDeviceToDevice hipMemcpyDeviceToDevice
+#endif
+
 typedef unsigned long ulong;
 
-extern "C" void *gpu_malloc(ulong size)
+EXTERN_C void *gpu_malloc(ulong size)
 {
     void *ptr;
-    cudaMalloc(&ptr, size);
+    gpuMalloc(&ptr, size);
     return ptr;
 }
 
-extern "C" void gpu_free(void *ptr)
+EXTERN_C void gpu_free(void *ptr)
 {
-    cudaFree(&ptr);
+    gpuFree(&ptr);
 }
 
-extern "C" void gpu_memcpy_htod(void *dst, const void *src, ulong size)
+EXTERN_C void gpu_memcpy_htod(void *dst, const void *src, ulong size)
 {
-    cudaMemcpy(dst, src, size, cudaMemcpyHostToDevice);
+    gpuMemcpy(dst, src, size, gpuMemcpyHostToDevice);
 }
 
-extern "C" void gpu_memcpy_dtoh(void *dst, const void *src, ulong size)
+EXTERN_C void gpu_memcpy_dtoh(void *dst, const void *src, ulong size)
 {
-    cudaMemcpy(dst, src, size, cudaMemcpyDeviceToHost);
+    gpuMemcpy(dst, src, size, gpuMemcpyDeviceToHost);
 }
 
-extern "C" void gpu_memcpy_dtod(void *dst, const void *src, ulong size)
+EXTERN_C void gpu_memcpy_dtod(void *dst, const void *src, ulong size)
 {
-    cudaMemcpy(dst, src, size, cudaMemcpyDeviceToDevice);
+    gpuMemcpy(dst, src, size, gpuMemcpyDeviceToDevice);
 }
 
 // Adapted from:
@@ -65,17 +85,17 @@ static __global__ void vec_max_f64_kernel(const double *in, ulong N, double *out
     }
 }
 
-extern "C" void gpu_vec_max_f64(const double *vec, ulong size, double *result)
+EXTERN_C void gpu_vec_max_f64(const double *vec, ulong size, double *result)
 {
     if (size == 0) {
         return;
     }
     double* block_max;
-    cudaMalloc(&block_max, sizeof(double) * REDUCE_GRID_SIZE);
+    gpuMalloc(&block_max, sizeof(double) * REDUCE_GRID_SIZE);
 
     vec_max_f64_kernel<<<REDUCE_GRID_SIZE, REDUCE_BLOCK_SIZE>>>(vec, size, block_max);
     vec_max_f64_kernel<<<1, REDUCE_BLOCK_SIZE>>>(block_max, REDUCE_GRID_SIZE, block_max);
 
-    cudaMemcpy(result, block_max, sizeof(double), cudaMemcpyDeviceToDevice);
-    cudaFree(block_max);
+    gpuMemcpy(result, block_max, sizeof(double), gpuMemcpyDeviceToDevice);
+    gpuFree(block_max);
 }
