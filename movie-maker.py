55c8883f1f8737c93f3b7a700abbbe4783eeeab5
Jack Hu <zhongth@g.clemson.edu>
2021-08-16 09:30:15 -0400

    Apply plot_patch to movie-maker plotting scheme

--- movie-maker.py
+++ movie-maker.py
@@ -17,18 +17,17 @@ def file_load(indir, outdir, savefigbool, filename):
 
     for name in Path(indir).iterdir():
         file_count += 1
-        chkpt = msgpack.load(open(name, 'rb'))
-        ni = chkpt['mesh']['ni']
-        nj = chkpt['mesh']['nj']
-        x0 = chkpt['mesh']['x0']
-        y0 = chkpt['mesh']['y0']
-        x1 = chkpt['mesh']['dx'] * chkpt['mesh']['ni'] + x0
-        y1 = chkpt['mesh']['dy'] * chkpt['mesh']['nj'] + y0
-        primitive = np.reshape(chkpt['primitive'], (ni + 4, nj + 4, 3))[2:-2,
-                    2:-2]
-        plt.figure(figsize=[12, 9.5])
-        plt.imshow(primitive[:, :, 0].T ** 0.25, origin='lower', cmap='plasma',
-                   extent=[x0, x1, y0, y1])
+        chkpt = msgpack.load(open(filename, 'rb'))
+        mesh = chkpt['mesh']
+        prim = np.zeros([mesh['ni'], mesh['nj'], 3])
+        for patch in chkpt['primitive_patches']:
+            i0 = patch['rect'][0]['start']
+            j0 = patch['rect'][1]['start']
+            i1 = patch['rect'][0]['end']
+            j1 = patch['rect'][1]['end']
+            local_prim = np.array(np.frombuffer(patch['data'])).reshape([i1 - i0, j1 - j0, 3])
+            prim[i0:i1, j0:j1] = local_prim
+        plt.imshow(prim[:,:,0].T, origin='lower')
         plt.colorbar()
         plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
         plt.title(r"{} $\Sigma^{{1/4}}$".format(name))
