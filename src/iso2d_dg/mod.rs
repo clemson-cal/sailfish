ac99043550e7d07671f876f31cdf1ce2e344bfdc
macfadyen <macfadyen@nyu.edu>
2021-09-13 08:00:05 -0400

    add Rust interface to DG solver

--- src/iso2d_dg/mod.rs
+++ src/iso2d_dg/mod.rs
@@ -4,14 +4,22 @@ pub mod solver;
 pub mod node;
 
 extern "C" {
-    pub fn iso2d_dg_say_hello(order: i32) -> i32;
-    pub fn iso2d_dg_get_order(cell: node::Cell) -> i32;
 
     pub fn iso2d_dg_primitive_to_weights(
         cell: node::Cell,
         mesh: StructuredMesh,
-        primitive_ptr: *const f64,
+        weights_ptr: *const f64,
         weights_ptr: *mut f64,
+        mode: ExecutionMode,
+    );
+
+    pub fn iso2d_dg_advance_rk(
+        cell: node::Cell,
+        mesh: StructuredMesh,
+        weights_rd_ptr: *const f64,
+        weights_wr_ptr: *mut f64,
+        eos: EquationOfState,
+        dt: f64,
         mode: ExecutionMode,
     );
 }
