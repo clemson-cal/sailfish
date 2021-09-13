9082ff165f559d44743476372581022f268c420e
macfadyen <macfadyen@nyu.edu>
2021-09-12 23:44:39 -0400

    Add basic DG scheme

--- src/iso2d_dg/mod.c
+++ src/iso2d_dg/mod.c
@@ -29,7 +29,7 @@ struct NodeData {
     real phi[MAX_POLYNOMIALS];
     real dphi_dx[MAX_POLYNOMIALS];
     real dphi_dy[MAX_POLYNOMIALS];
-    real weight;
+    real weight; 
 };
 
 
@@ -112,6 +112,18 @@ static __host__ __device__ void primitive_to_conserved(const real *prim, real *c
     cons[2] = py;
 }
 
+static __host__ __device__ void conserved_to_primitive(const real *cons, real *prim)
+{
+    real rho = cons[0];
+    real px = cons[1];
+    real py = cons[2];
+    real vx = px / rho;
+    real vy = py / rho;
+
+    prim[0] = rho;
+    prim[1] = vx;
+    prim[2] = vy;
+}
 
 // ============================ SCHEME ========================================
 // ============================================================================
@@ -153,6 +165,163 @@ static __host__ __device__ void primitive_to_weights_zone(
     }
 }
 
+static __host__ __device__ void advance_rk_zone_dg(
+    struct Cell cell,
+    struct Mesh mesh,
+    struct Patch weights_rd,
+    struct Patch weights_wr,
+    struct EquationOfState eos,
+    struct BoundaryCondition bc,
+    real dt,
+    int i,
+    int j)
+{
+    real dx = mesh.dx;
+    real dy = mesh.dy;
+
+    int n_quad = num_quadrature_points(cell);
+    int n_poly = num_polynomials(cell);
+    int n_face = cell.order;
+
+    real *wij = GET(weights_rd, i, j);
+    real *wli = GET(weights_rd, i - 1, j);
+    real *wri = GET(weights_rd, i + 1, j);
+    real *wlj = GET(weights_rd, i, j - 1);
+    real *wrj = GET(weights_rd, i, j + 1);
+
+    real ulip[NCONS];
+    real ulim[NCONS];
+    real urip[NCONS];
+    real urim[NCONS];
+    real uljp[NCONS];
+    real uljm[NCONS];
+    real urjp[NCONS];
+    real urjm[NCONS];
+
+    real plip[NCONS];
+    real plim[NCONS];
+    real prip[NCONS];
+    real prim[NCONS];
+    real pljp[NCONS];
+    real pljm[NCONS];
+    real prjp[NCONS];
+    real prjm[NCONS];
+
+    real fli[NCONS];
+    real fri[NCONS];
+    real flj[NCONS];
+    real frj[NCONS];
+
+    real dwij[NCONS * n_poly];
+
+    for (int q = 0; q < NCONS; ++q)
+    {
+        for (int l = 0; l < n_poly; ++l)
+        {
+            dwij[q * n_poly + l] = 0.0;  
+        }
+    }
+
+    // surface term
+    for (int qp = 0; qp < n_face; ++qp)
+    {
+        for (int q = 0; q < NCONS; ++q)
+        {
+            ulim[q] = 0.0;
+            ulip[q] = 0.0;
+            urim[q] = 0.0;
+            urip[q] = 0.0;
+            uljm[q] = 0.0;
+            uljp[q] = 0.0;
+            urjm[q] = 0.0;
+            urjp[q] = 0.0;
+
+            for (int l = 0; l < n_poly; ++l)
+            {
+                ulim[q] += wli[q * n_poly + l] * cell.faceri[qp].phi[l]; // right face of zone i-1 
+                ulip[q] += wij[q * n_poly + l] * cell.faceli[qp].phi[l]; // left face of zone i
+                urim[q] += wij[q * n_poly + l] * cell.faceri[qp].phi[l]; // right face of zone i  
+                urip[q] += wri[q * n_poly + l] * cell.faceli[qp].phi[l]; // left face of zone i + 1
+                uljm[q] += wlj[q * n_poly + l] * cell.facerj[qp].phi[l]; // top face of zone j-1 
+                uljp[q] += wij[q * n_poly + l] * cell.facelj[qp].phi[l]; // bottom face of zone j
+                urjm[q] += wij[q * n_poly + l] * cell.facerj[qp].phi[l]; // top face of zone j  
+                urjp[q] += wrj[q * n_poly + l] * cell.facelj[qp].phi[l]; // bottom face of zone j + 1                       
+            }
+        }
+
+        conserved_to_primitive(ulim, plim);
+        conserved_to_primitive(ulip, plip);
+        conserved_to_primitive(urim, prim);
+        conserved_to_primitive(urip, prip);
+        conserved_to_primitive(uljm, pljm);
+        conserved_to_primitive(uljp, pljp);
+        conserved_to_primitive(urjm, prjm);
+        conserved_to_primitive(urjp, prjp);
+
+        riemann_hlle(plim, plip, fli, 0);
+        riemann_hlle(prim, prip, fri, 0);
+        riemann_hlle(pljm, pljp, flj, 1);
+        riemann_hlle(prjm, prjp, frj, 1);
+        
+        for (int q = 0; q < NCONS; ++q)
+        {
+            for (int l = 0; l < n_poly; ++l)
+            {
+                dwij[q * n_poly + l] -= fli[q] * cell.faceli[qp].phi[l] * cell.faceli[qp].gw;
+                dwij[q * n_poly + l] -= fri[q] * cell.faceri[qp].phi[l] * cell.faceri[qp].gw;
+                dwij[q * n_poly + l] -= flj[q] * cell.facelj[qp].phi[l] * cell.facelj[qp].gw;
+                dwij[q * n_poly + l] -= frj[q] * cell.facerj[qp].phi[l] * cell.facerj[qp].gw;  
+            }
+        }
+    }
+
+    real cons[NCONS];
+    real prim[NCONS];
+    real flux_x[NCONS];
+    real flux_y[NCONS];
+
+    // volume term
+    for (int qp = 0; qp < n_quad; ++qp)
+    {
+        struct NodeData node = cell.interior_nodes[qp];
+
+        for (int q = 0; q < NCONS; ++q)
+        {
+            cons[q] = 0.0;
+
+            for (int l = 0; l < NK; ++l)
+            {
+                cons[q] += wij[q * n_poly + l] * node.phi[l];
+            }
+        }
+
+        conserved_to_primitive(cons, prim);
+
+        primitive_to_flux_vector(prim, flux_x, 0);
+        primitive_to_flux_vector(prim, flux_y, 1);
+
+        for (int q = 0; q < NCONS; ++q)
+        {
+            for (int l = 0; l < n_poly; ++l)
+            {
+                dwij[q * n_poly + l] += flux_x[q] * node.dphidx[l] * node.gw;
+                dwij[q * n_poly + l] += flux_y[q] * node.dphidy[l] * node.gw; 
+            }
+        }
+    }
+
+
+    real *wout = GET(weights_wr, i, j);
+
+    for (int q = 0; q < NCONS; ++q)
+    {
+        for (int l = 0; l < n_poly; ++l)
+        {
+            wout[q * n_poly + l] = wij[q * n_poly + l] + 0.5 * dwij[q * n_poly + l] * dt / dx; //assumes dy=dx
+        }
+    }
+
+}
 
 // ============================ KERNELS =======================================
 // ============================================================================
@@ -170,6 +339,32 @@ static void __global__ primitive_to_weights_kernel(
     if (i < mesh.ni && j < mesh.nj)
     {
         primitive_to_weights_zone(cell, primitive, weights, i, j);
+    }
+}
+
+static void __global__ advance_rk_kernel_dg(
+    struct Mesh mesh,
+    struct Patch weights_rd,
+    struct Patch weights_wr,
+    struct EquationOfState eos,
+    struct BoundaryCondition buffer,
+    real dt
+    )
+{
+    int i = threadIdx.y + blockIdx.y * blockDim.y;
+    int j = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (i < mesh.ni && j < mesh.nj)
+    {
+        advance_rk_zone_dg(
+            mesh,
+            weights_rd,
+            weights_wr,
+            eos,
+            buffer,
+            dt,
+            i, j
+        );
     }
 }
 
