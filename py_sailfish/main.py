36b5782720836ecda6249ea1eab5429f05dc52c8
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-11-22 16:47:38 -0500

    Py: fix FFI arg conversion: ctypes and cupy work differently

--- py_sailfish/main.py
+++ py_sailfish/main.py
@@ -1,4 +1,4 @@
-from ctypes import c_double, c_int, POINTER, CDLL
+from ctypes import c_double, c_int, cast, POINTER, CDLL
 import time
 import glob
 import numpy as np
@@ -9,9 +9,13 @@ block_size = 64
 
 class Library:
     def __init__(self, mode='cpu'):
+        self.mode = mode
+        self.load_lib()
+
+    def load_lib(self):
         with open('src/srhd_1d.c', 'r') as srcfile:
             code = srcfile.read()
-        if mode == 'cpu':
+        if self.mode == 'cpu':
             import cffi
             ffi = cffi.FFI()
             ffi.set_source(
@@ -24,33 +28,47 @@ class Library:
             target = 'lib/srhd_1d.so'
             target = ffi.compile(target=target)
             self.module = CDLL(target)
-        if mode == 'gpu':
+        if self.mode == 'gpu':
             import cupy
             module = cupy.RawModule(code=code, options=('-D EXEC_MODE=2',))
             module.compile()
             self.module = module
-        self.mode = mode
 
     def invoke(self, symbol, num_zones, args):
+        converted_args = [self.convert(arg) for arg in args]
         if self.mode == 'cpu':
-            function = getattr(self.module, symbol)
-            return function(*args)
+            kernel = getattr(self.module, symbol)
+            return kernel(*converted_args)
         if self.mode == 'gpu':
-            num_blocks = ((num_zones + block_size - 1) // block_size,)
-            block_size = (block_size,)
-            function = self.module.get_function(symbol)
-            return function(num_blocks, block_size, args)
+            nb = ((num_zones + block_size - 1) // block_size,)
+            bs = (block_size,)
+            kernel = self.module.get_function(symbol)
+            return kernel(nb, bs, converted_args)
+
+    def convert(self, arg):
+        if self.mode == 'cpu':
+            if type(arg) == int:
+                return c_int(arg)
+            if type(arg) == float:
+                return c_double(arg)
+            if type(arg) == np.ndarray:
+                assert(arg.dtype == float)
+                return arg.ctypes.data_as(POINTER(c_double))
+        if self.mode == 'gpu':
+            import cupy
+            if type(arg) == int:
+                return arg
+            if type(arg) == float:
+                return arg
+            if type(arg) == cupy.ndarray:
+                assert(arg.dtype == float)
+                return arg.data.ptr
+        raise ValueError("kernel arguments must be int, float, or ndarray[float]")
 
 
 class Memory:
     def __init__(self, mode='cpu'):
         self.mode = mode
-
-    def ptr(self, a):
-        if self.mode == 'cpu':
-            return a.ctypes.data_as(POINTER(c_double))
-        if self.mode == 'gpu':
-            return a.data.ptr
 
     def copy_to_host(self, a):
         if self.mode == 'cpu':
@@ -92,9 +110,9 @@ class Memory:
 Adapter class to drive the srhd_1d C extension module.
 """
 class Solver:
-    def __init__(self, primitive, time=0.0, bc='inflow', coords='cartesian'):
-        self.lib = Library(mode='cpu')
-        self.mem = Memory(mode='cpu')
+    def __init__(self, primitive, time=0.0, bc='inflow', coords='cartesian', mode='cpu'):
+        self.lib = Library(mode=mode)
+        self.mem = Memory(mode=mode)
         self.num_zones = primitive.shape[0]
         self.faces = self.mem.linspace(0.0, 1.0, num_zones + 1)
         self.boundary_condition = dict(inflow=0, zeroflux=1)[bc]
@@ -113,24 +131,24 @@ class Solver:
     def primitive_to_conserved(self, primitive):
         conserved = self.mem.zeros_like(primitive)
         args = (
-            c_int(self.num_zones),
-            self.mem.ptr(self.faces),
-            self.mem.ptr(primitive),
-            self.mem.ptr(conserved),
-            c_double(self.scale_factor()),
-            c_int(self.coords),
+            self.num_zones,
+            self.faces,
+            primitive,
+            conserved,
+            self.scale_factor(),
+            self.coords,
         )
         self.lib.invoke('srhd_1d_primitive_to_conserved', self.num_zones, args)
         return conserved
 
     def recompute_primitive(self):
         args = (
-            c_int(self.num_zones),
-            self.mem.ptr(self.faces),
-            self.mem.ptr(self.conserved1),
-            self.mem.ptr(self.primitive1),
-            c_double(self.scale_factor()),
-            c_int(self.coords),
+            self.num_zones,
+            self.faces,
+            self.conserved1,
+            self.primitive1,
+            self.scale_factor(),
+            self.coords,
         )
         self.lib.invoke('srhd_1d_conserved_to_primitive', self.num_zones, args)
 
@@ -141,19 +159,19 @@ class Solver:
     def advance_rk(self, rk_param, dt):
         self.recompute_primitive()
         args = (
-            c_int(self.num_zones),
-            self.mem.ptr(self.faces),
-            self.mem.ptr(self.conserved0),
-            self.mem.ptr(self.primitive1),
-            self.mem.ptr(self.conserved1),
-            self.mem.ptr(self.conserved2),
-            c_double(self.scale_factor_initial),
-            c_double(self.scale_factor_derivative),
-            c_double(self.time),
-            c_double(rk_param),
-            c_double(dt),
-            c_int(self.coords),
-            c_int(self.boundary_condition),
+            self.num_zones,
+            self.faces,
+            self.conserved0,
+            self.primitive1,
+            self.conserved1,
+            self.conserved2,
+            self.scale_factor_initial,
+            self.scale_factor_derivative,
+            self.time,
+            rk_param,
+            dt,
+            self.coords,
+            self.boundary_condition,
         )
         self.lib.invoke('srhd_1d_advance_rk', self.num_zones, args)
         self.time = self.time0 * rk_param + (self.time0 + dt) * (1.0 - rk_param)
@@ -166,7 +184,7 @@ class Solver:
 
 
 if __name__ == "__main__":
-    num_zones = 1000000
+    num_zones = 10000
     fold = 100
     cfl_number = 0.6
     dt = 1.0 / num_zones * cfl_number
@@ -183,7 +201,7 @@ if __name__ == "__main__":
             primitive[i, 0] = 0.1
             primitive[i, 2] = 0.125
 
-    solver = Solver(primitive)
+    solver = Solver(primitive, mode='cpu')
 
     while solver.time < 0.01:
         start = time.perf_counter()
