c48eabf34b48fd29971ec0078970663e0dba1aa5
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-12-02 10:18:09 -0500

    Use the __pycache__ directory for compiled libraries

--- py_sailfish/sailfish/library.py
+++ py_sailfish/sailfish/library.py
@@ -1,7 +1,8 @@
 import logging
-import os
 import hashlib
 import time
+from os import listdir
+from os.path import join, splitext, basename, dirname
 from ctypes import c_double, c_int, POINTER, CDLL
 from sailfish.system import build_config, measure_time
 from sailfish.parse_api import parse_api
@@ -38,8 +39,8 @@ class Library:
     """
 
     def __init__(self, module_file, mode="cpu", debug=True):
-        abs_path, _ = os.path.splitext(module_file)
-        module = os.path.basename(abs_path)
+        abs_path, _ = splitext(module_file)
+        module = basename(abs_path)
 
         logger.info(f"debug mode {'enabled' if debug else 'disabled'}")
         logger.info(f"prepare module {module} for {mode} execution")
@@ -54,54 +55,58 @@ class Library:
                 code = srcfile.read()
 
             if self.cpu_mode:
-                import cffi
-                import numpy
-
-                define_macros = [("EXEC_MODE", dict(cpu=0, omp=1)[mode])]
-                ffi = cffi.FFI()
-                ffi.set_source(
-                    module,
-                    code,
-                    define_macros=define_macros,
-                    extra_compile_args=build_config["extra_compile_args"],
-                    extra_link_args=build_config["extra_link_args"],
-                )
-
-                # Build a hash for the compiled library based on code, define
-                # macros, and build args.
-                sha = hashlib.sha256()
-                sha.update(code.encode("utf-8"))
-                sha.update(str(define_macros).encode("utf-8"))
-                sha.update(str(build_config).encode("utf-8"))
-                cache_dir = os.path.join(".sailfish", sha.hexdigest())
-
-                try:
-                    so_name = os.path.join(
-                        cache_dir,
-                        next(
-                            filter(lambda f: f.endswith(".so"), os.listdir(cache_dir))
-                        ),
-                    )
-                    self.module = CDLL(so_name)
-                    logger.info(f"load cached library")
-                except (FileNotFoundError, StopIteration) as e:
-                    target = ffi.compile(cache_dir)
-                    self.module = CDLL(target)
-                    logger.info(f"recompile library")
-
-                self.xp = numpy
+                self.load_cpu_module(code, module, mode=mode)
             else:
-                import cupy
-
-                module = cupy.RawModule(code=code, options=("-D EXEC_MODE=2",))
-                module.compile()
-                self.module = module
-                self.xp = cupy
+                self.load_gpu_module(code)
 
             logger.info(f"module preparation took {prep_time():0.3}s")
 
         for symbol in self.api:
             logger.info(f"+-- {symbol}")
+
+    def load_cpu_module(self, code, module, mode="cpu"):
+        import cffi
+        import numpy
+
+        define_macros = [("EXEC_MODE", dict(cpu=0, omp=1)[mode])]
+        ffi = cffi.FFI()
+        ffi.set_source(
+            module,
+            code,
+            define_macros=define_macros,
+            extra_compile_args=build_config["extra_compile_args"],
+            extra_link_args=build_config["extra_link_args"],
+        )
+
+        # Build a hash for the compiled library based on code, define
+        # macros, and build args.
+        sha = hashlib.sha256()
+        sha.update(code.encode("utf-8"))
+        sha.update(str(define_macros).encode("utf-8"))
+        sha.update(str(build_config).encode("utf-8"))
+        cache_dir = join(dirname(__file__), "__pycache__", sha.hexdigest())
+
+        try:
+            so_name = join(
+                cache_dir,
+                next(filter(lambda f: f.endswith(".so"), listdir(cache_dir))),
+            )
+            self.module = CDLL(so_name)
+            logger.info(f"load cached library")
+        except (FileNotFoundError, StopIteration) as e:
+            target = ffi.compile(cache_dir)
+            self.module = CDLL(target)
+            logger.info(f"recompile library")
+
+        self.xp = numpy
+
+    def load_gpu_module(self, code):
+        import cupy
+
+        module = cupy.RawModule(code=code, options=("-D EXEC_MODE=2",))
+        module.compile()
+        self.module = module
+        self.xp = cupy
 
     def __getattr__(self, symbol):
         arg_format = self.api[symbol]
@@ -113,8 +118,8 @@ class Library:
 
         def invoke_kernel(*args):
             if self.debug:
-                validate_constraints(args, arg_format, symbol)
                 validate_types(args, arg_format, symbol, self.xp)
+                validate_constraints(args, arg_format, symbol)
 
             if self.cpu_mode:
                 kernel(*to_ctypes(args, arg_format))
@@ -128,6 +133,11 @@ class Library:
 
 
 def to_ctypes(args, arg_format):
+    """
+    Coerce a sequence of values to their appropriate ctype.
+
+    The expected type is determined from the `arg_format` list.
+    """
     for arg, (typename, _, _) in zip(args, arg_format):
         if typename == "int":
             yield c_int(arg)
@@ -137,35 +147,43 @@ def to_ctypes(args, arg_format):
             yield arg.ctypes.data_as(POINTER(c_double))
 
 
+def type_error(sym, n, a, b):
+    return TypeError(f"argument {n} to {sym} has type {type(a).__name__}, expected {b}")
+
+
+def dtype_error(sym, n, a, b):
+    return TypeError(f"argument {n} to {sym} has dtype {a.dtype}, expected {b}")
+
+
+def arglen_error(sym, a, b):
+    return TypeError(f"{sym} takes exactly {len(b)} arguments ({len(a)} given)")
+
+
 def validate_types(args, arg_format, symbol, xp):
     if len(args) != len(arg_format):
-        raise TypeError(
-            f"{symbol} takes exactly {len(arg_format)} arguments ({len(args)} given)"
-        )
+        raise arglen_error(symbol, args, arg_format)
 
     for n, (arg, (typename, argname, constraint)) in enumerate(zip(args, arg_format)):
         if typename == "int":
             if type(arg) is not int:
-                raise TypeError(
-                    f"argument {n} to {symbol} has type {type(arg)}, expected int"
-                )
+                raise type_error(symbol, n, arg, "int")
         elif typename == "double":
             if type(arg) is not float:
-                raise TypeError(
-                    f"argument {n} to {symbol} has type {type(arg)}, expected float64"
-                )
+                raise type_error(symbol, n, arg, "float64")
         elif typename == "double*":
             if type(arg) is not xp.ndarray:
-                raise TypeError(
-                    f"argument {n} to {symbol} has type {type(arg)}, expected ndarray"
-                )
+                raise type_error(symbol, n, arg, "ndarray")
             if arg.dtype != xp.float64:
-                raise TypeError(
-                    f"argument {n} to {symbol} has dtype {arg.dtype}, expected float64"
-                )
+                raise dtype_error(symbol, n, arg, "float64")
 
 
 def validate_constraints(args, arg_format, symbol):
+    """
+    Validate kernel argument constraints for a symbol.
+
+    Constraints are (optionally defined in C code and extracted in the
+    `parse_api` module).
+    """
     scope = dict(zip([a[1] for a in arg_format], args))
     for arg, (_, name, constraint) in zip(args, arg_format):
         if constraint:
