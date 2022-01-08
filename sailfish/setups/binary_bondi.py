ee4800af3cfdf4d0e21c0a043eee630d2836401a
Binayyak Roy <binayyakroy@Haze.local>
2022-01-08 02:00:43 -0500

    Final draft for binary-bondi setup

--- sailfish/setups/binary_bondi.py
+++ sailfish/setups/binary_bondi.py
@@ -1,6 +1,7 @@
 """
 For simulating bondi-hoyle-lyttleton accretion in a binary in a wind setup.
 """
+from typing import Callable
 from sailfish.mesh import PlanarCartesian2DMesh
 from sailfish.physics.circumbinary import SinkModel, PointMass
 from sailfish.physics.kepler import OrbitalElements
@@ -14,31 +15,72 @@ class BinaryBondi(Setup):
     x_bin = param(
         0.0, "horizontal position of binary COM (with respect to domain centroid)"
     )
-    bh_sep = param(1.0, "black hole separation")
+    mach_number = param(10.0, "orbital Mach number (isothermal)", mutable=True)
+    bh_sep = param(0.25, "black hole separation")
     bh_mass = param(1.0, "black hole's mass")
-    bh_rad = param(5e-2, "black hole's radius")  # sink_radius
+    sink_radius = param(5e-2, "black hole's (sink) radius")
     sink_model = param(SinkModel.ACCELERATION_FREE, "sink prescription")
     sink_rate = param(1e2, "sink rate")
-    q = param(1.0, "system mass ratio: [0.0-1.0]")
-    e = param(0.0, "eccentricity")
+    mass_ratio = param(1.0, "system mass ratio: [0.0-1.0]")
+    eccentricity = param(0.0, "eccentricity")
+    softening_length = param(0.05, "gravitational softening length", mutable=True)
 
     def primitive(self, t, coords, primitive):
         primitive[0] = 1.0
-        primitive[1] = self.physics.mach_number * self.physics.sound_speed
+        primitive[1] = self.mach_number
+        primitive[2] = 0.0
 
     def mesh(self, resolution):
-        if isinstance(aspect, int):
-            return PlanarCartesian2DMesh.centered_rectangle(
-                self.height, resolution, int(self.aspect)
-            )
-        else:
-            raise ValueError("Aspect has to be supplied an integer!")
+        return PlanarCartesian2DMesh.centered_rectangle(
+            self.height, resolution, int(self.aspect)
+        )
 
-    def masses(self):
-        m1, m2 = OrbitalElements(
-            1.0, self.bh_mass * (1 + self.q), self.q, self.e
-        ).orbital_state
+    @property
+    def default_resolution(self):
+        return 1000
 
     @property
     def physics(self):
-        return dict()
+        return dict(
+            mach_number=self.mach_number,
+            point_mass_function=self.point_masses,
+        )
+
+    @property
+    def solver(self):
+        return "cbdiso_2d"
+
+    @property
+    def boundary_condition(self):
+        return "outflow"
+
+    @property
+    def default_end_time(self):
+        return 10.0
+
+    def point_masses(self, time):
+        elements = OrbitalElements(
+            semimajor_axis=self.bh_sep,
+            total_mass=self.bh_mass * (1 + self.mass_ratio),
+            mass_ratio=self.mass_ratio,
+            eccentricity=self.eccentricity,
+        )
+        m1, m2 = elements.orbital_state(time)
+        m1 = PointMass(**m1._asdict())
+        m2 = PointMass(**m2._asdict())
+        return (
+            m1._replace(
+                position_x=m1.position_x + self.x_bin,
+                softening_length=self.softening_length,
+                sink_model=self.sink_model,
+                sink_rate=self.sink_rate,
+                sink_radius=self.sink_radius,
+            ),
+            m2._replace(
+                position_x=m2.position_x + self.x_bin,
+                softening_length=self.softening_length,
+                sink_model=self.sink_model,
+                sink_rate=self.sink_rate,
+                sink_radius=self.sink_radius,
+            ),
+        )
