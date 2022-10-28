dce3aae126a7917908adb69ef93dd86f203c00bb
Jonathan Zrake <jonathan.zrake@gmail.com>
2022-10-28 12:01:46 -0400

    Update upsample script

--- scripts/upsample.py
+++ scripts/upsample.py
@@ -1,22 +1,27 @@
-import numpy as np
-import pickle
-import sys
+from pickle import load, dump
+from sys import path, argv
+from pathlib import Path
+
+path.append(str(Path(__file__).parent.parent))
 
 
 def scale_by_n(infile, n):
     with open(infile, "rb") as inf:
-        chk = pickle.load(inf)
-    chk["solution"] = chk["solution"].repeat(n, axis=0).repeat(n, axis=1)
-    chk["driver"] = chk["driver"]._replace(resolution=chk["driver"].resolution * n)
-    chk["mesh"] = chk["mesh"]._replace(ni=chk["mesh"].ni * n, nj=chk["mesh"].nj * n)
-    outfile = infile[:-3] + "_upscaled.pk"
-    with open(outfile, "wb") as outf:
-        pickle.dump(chk, outf)
-    print(f"Written {infile} scaled by {n} to {outfile}")
+        c = load(inf)
+
+    c["solution"] = c["solution"].repeat(n, axis=0).repeat(n, axis=1)
+    c["driver"] = c["driver"]._replace(resolution=c["driver"].resolution * n)
+    c["mesh"] = c["mesh"]._replace(ni=c["mesh"].ni * n, nj=c["mesh"].nj * n)
+    outfile = infile.replace(".pk", "_upsampled.pk")
+
+    with open(outfile, "wb") as f:
+        dump(c, f)
+
+    print(f"write {infile} scaled by {n}x to {outfile}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(argv) < 2:
         print("No files provided, use 'python3 upsample.py file1 file2 ...'")
-    for file in sys.argv[1:]:
+    for file in argv[1:]:
         scale_by_n(file, 2)
