6939a165c1fd3718e37c988e020f08d905ec6600
Jonathan Zrake <jonathan.zrake@gmail.com>
2022-11-10 09:05:57 -0500

    Write a quick multi-patch option to euler_plm (bug?)

--- ideas/euler_plm.py
+++ ideas/euler_plm.py
@@ -12,6 +12,8 @@ def measure_time() -> float:
 def main():
     from argparse import ArgumentParser
     from new_kernels import configure_kernel_module
+    from hydro_euler import EulerEquations
+    from gradient_estimation import plm_gradient_1d, plm_gradient_2d, extrapolate
 
     parser = ArgumentParser()
     parser.add_argument(
@@ -180,6 +182,91 @@ def main():
 
             plt.imshow(p[:, :, 0])
             plt.colorbar()
+            plt.show()
+
+    elif args.dim == 0:
+        from grid import (
+            initial_patches,
+            cell_center_coordinates,
+            initial_data,
+            copy_guard_zones,
+        )
+
+        num_zones = 10
+        dx = 1.0 / num_zones
+        fold = 1
+        dt = dx * 1e-1
+
+        hydro = EulerEquations(dim=2, gamma_law_index=5.0 / 3.0)
+        patches = set(initial_patches(4, 4))
+        coordinate = {
+            ij: cell_center_coordinates(*ij, 4, 4, num_zones, num_zones)
+            for ij in patches
+        }
+
+        primitives = dict()
+
+        for i, j in patches:
+            x, y = coordinate[(i, j)]
+            disk = (x**2 + y**2) ** 0.5 < 0.1
+            fisk = logical_not(disk)
+            p = zeros(x.shape + (hydro.ncons,))
+            p[disk, 0] = 1.000
+            p[fisk, 0] = 0.100
+            p[disk, 3] = 1.000
+            p[fisk, 3] = 0.125
+            primitives[(i, j)] = p
+
+        t = 0.0
+        n = 0
+
+        while t < 0.05:
+            with measure_time() as fold_time:
+                copy_guard_zones(primitives)
+                for _ in range(fold):
+                    for i, j in patches:
+                        p = primitives[(i, j)]
+                        u = zeros_like(p)
+                        gx = zeros_like(p)
+                        gy = zeros_like(p)
+                        pp = zeros_like(p)
+                        pm = zeros_like(p)
+                        fhat = zeros((num_zones + 4 - 1, num_zones + 4, hydro.ncons))
+                        ghat = zeros((num_zones + 4, num_zones + 4 - 1, hydro.ncons))
+
+                        plm_gradient_2d(p, gx, gy, 1.5)
+
+                        extrapolate(p, gx, pm, pp)
+                        pl = pp[:-1, :].copy()
+                        pr = pm[+1:, :].copy()
+                        hydro.riemann_hlle(pl, pr, fhat, 1)
+
+                        extrapolate(p, gy, pm, pp)
+                        pl = pp[:, :-1].copy()
+                        pr = pm[:, +1:].copy()
+                        hydro.riemann_hlle(pl, pr, ghat, 2)
+
+                        hydro.prim_to_cons(p, u)
+                        u[1:-1, :] -= diff(fhat, axis=0) * (dt / dx)
+                        u[:, 1:-1] -= diff(ghat, axis=1) * (dt / dx)
+                        hydro.cons_to_prim(u, p)
+
+                    t += dt
+                    n += 1
+
+            kzps = num_zones * num_zones * len(patches) / fold_time() * 1e-3 * fold
+            print(f"[{n:04d}]: t={t:.4f} Mzps={kzps * 1e-3:.3f}")
+
+        if args.plot:
+            from matplotlib import pyplot as plt
+
+            for i, j in patches:
+                z = primitives[(i, j)][:, :, 0]
+                x, y = coordinate[(i, j)]
+
+                plt.pcolormesh(x, y, z, vmin=0, vmax=1)
+
+            plt.axis("equal")
             plt.show()
 
 
--- ideas/grid.py
+++ ideas/grid.py
@@ -1,59 +1,72 @@
 from numpy import zeros, linspace, meshgrid, exp
 
 
-grid = dict()
-dx = 0.125
-dy = 0.125
-ddx = dx / 10
-ddy = dy / 10
+def copy_guard_zones(grid):
+    for i, j in grid:
+        cc = grid.get((i, j))
+        lc = grid.get((i - 1, j), None)
+        rc = grid.get((i + 1, j), None)
+        cl = grid.get((i, j - 1), None)
+        cr = grid.get((i, j + 1), None)
 
-for i in range(8):
-    for j in range(8):
-        x0 = -0.5 + (i + 0) * dx
-        x1 = -0.5 + (i + 1) * dx
-        y0 = -0.5 + (j + 0) * dy
-        y1 = -0.5 + (j + 1) * dy
-        xv = linspace(x0 - 2 * ddx, x1 + 2 * ddy, 15)
-        yv = linspace(y0 - 2 * ddx, y1 + 2 * ddy, 15)
-        xc = 0.5 * (xv[1:] + xv[:-1])
-        yc = 0.5 * (yv[1:] + yv[:-1])
-        x, y = meshgrid(xc, yc, indexing="ij")
-        z = zeros([14, 14])
-        z[...] = exp(-(x**2 + y**2) / 0.01)
-        grid[(i, j)] = (x, y, z)
-
-for i, j in grid:
-    cc = grid.get((i, j))
-    lc = grid.get((i - 1, j), None)
-    rc = grid.get((i + 1, j), None)
-    cl = grid.get((i, j - 1), None)
-    cr = grid.get((i, j + 1), None)
-
-    if lc is not None:
-        cc[2][:+2, :] = lc[2][-4:-2, :]
-    if rc is not None:
-        cc[2][-2:, :] = rc[2][+2:+4, :]
-    if cl is not None:
-        cc[2][:, :+2] = cl[2][:, -4:-2]
-    if cr is not None:
-        cc[2][:, -2:] = cr[2][:, +2:+4]
+        if lc is not None:
+            cc[:+2, 2:-2] = lc[-4:-2, 2:-2]
+        if rc is not None:
+            cc[-2:, 2:-2] = rc[+2:+4, 2:-2]
+        if cl is not None:
+            cc[2:-2, :+2] = cl[2:-2, -4:-2]
+        if cr is not None:
+            cc[2:-2, -2:] = cr[2:-2, +2:+4]
 
 
-import matplotlib.pyplot as plt
+def cell_center_coordinates(i, j, ni_patches, nj_patches, ni, nj):
+    dx = 1.0 / ni_patches
+    dy = 1.0 / nj_patches
+    ddx = dx / ni
+    ddy = dy / nj
+    x0 = -0.5 + (i + 0) * dx
+    x1 = -0.5 + (i + 1) * dx
+    y0 = -0.5 + (j + 0) * dy
+    y1 = -0.5 + (j + 1) * dy
+    xv = linspace(x0 - 2 * ddx, x1 + 2 * ddy, ni + 5)
+    yv = linspace(y0 - 2 * ddx, y1 + 2 * ddy, nj + 5)
+    xc = 0.5 * (xv[1:] + xv[:-1])
+    yc = 0.5 * (yv[1:] + yv[:-1])
+    return meshgrid(xc, yc, indexing="ij")
 
-for i, j in grid:
-    if i % 2 == 0 or j % 2 == 0:
-        continue
 
-    g = grid[(i, j)]
+def initial_patches(ni_patches, nj_patches):
+    for i in range(ni_patches):
+        for j in range(nj_patches):
+            yield i, j
 
-    plt.pcolormesh(
-        g[0],
-        g[1],
-        g[2],
-        vmin=0,
-        vmax=1,
-    )
 
-plt.axis("equal")
-plt.show()
+def initial_data(x, y):
+    z = exp(-(x**2 + y**2) / 0.01)
+    z[:+2, :] = 0.0
+    z[-2:, :] = 0.0
+    z[:, :+2] = 0.0
+    z[:, -2:] = 0.0
+    return z
+
+
+if __name__ == "__main__":
+    patches = set(initial_patches(8, 8))
+    coordinate = {ij: cell_center_coordinates(*ij, 8, 8, 10, 10) for ij in patches}
+    primitives = {ij: initial_data(*xy) for ij, xy in coordinate.items()}
+
+    copy_guard_zones(primitives)
+
+    import matplotlib.pyplot as plt
+
+    for i, j in patches:
+        if i % 2 == 0 or j % 2 == 0:
+            continue
+
+        z = primitives[(i, j)]
+        x, y = coordinate[(i, j)]
+
+        plt.pcolormesh(x, y, z, vmin=0, vmax=1)
+
+    plt.axis("equal")
+    plt.show()
