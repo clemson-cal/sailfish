33406ad9f6be9263268a70b262d1a1e38ce6c56d
Jonathan Zrake <jonathan.zrake@gmail.com>
2022-11-18 22:59:34 -0500

    Fix typos in fmr, new tests, experimental flux correction

--- ideas/fmr_grid.py
+++ ideas/fmr_grid.py
@@ -170,10 +170,10 @@ def fill_guard_cl(index, grid):
 
     try:
         cl = grid[(level - 1, (i // 2, j // 2 - 1))]
-        if j % 2 == 0:
-            cc[ng:-ng, :ng] = upsample(cl[+ng : ng + ni // 2, ni : ni + 1])
+        if i % 2 == 0:
+            cc[ng:-ng, :ng] = upsample(cl[+ng : ng + ni // 2, nj : nj + 1])
         else:
-            cc[ng:-ng, :ng] = upsample(cl[ng + ni // 2 : -ng, ni : ni + 1])
+            cc[ng:-ng, :ng] = upsample(cl[ng + ni // 2 : -ng, nj : nj + 1])
         return
     except KeyError as e:
         if DEBUG_MODE:
@@ -182,23 +182,92 @@ def fill_guard_cl(index, grid):
     try:
         cl0 = grid[(level + 1, (2 * i + 0, 2 * (j - 1)))]
         cl1 = grid[(level + 1, (2 * i + 1, 2 * (j - 1)))]
-        cc[ng : +ng + nj // 2, :ng] = downsample(cl0[ng:-ng, -3 * ng : -ng])
-        cc[ng + nj // 2 : -ng, :ng] = downsample(cl1[ng:-ng, -3 * ng : -ng])
+        cc[ng : +ng + ni // 2, :ng] = downsample(cl0[ng:-ng, -3 * ng : -ng])
+        cc[ng + ni // 2 : -ng, :ng] = downsample(cl1[ng:-ng, -3 * ng : -ng])
         return
     except KeyError as e:
         if DEBUG_MODE:
             print(f"no neighbor {e} for block {index}")
 
 
+def correct_flux_rc(index, grid):
+    level, (i, j) = index
+    cc = grid[index]
+    nj = cc.shape[1]
+
+    rc0 = grid.get((level + 1, (2 * (i + 1), 2 * j + 0)), None)
+    rc1 = grid.get((level + 1, (2 * (i + 1), 2 * j + 1)), None)
+
+    if rc0 is not None:
+        cc[-1, : nj // 2] = 0.5 * (rc0[0, 0:-1:2] + rc0[0, 1::2])
+    if rc1 is not None:
+        cc[-1, nj // 2 :] = 0.5 * (rc1[0, 0:-1:2] + rc1[0, 1::2])
+
+
+def correct_flux_lc(index, grid):
+    level, (i, j) = index
+    cc = grid[index]
+    nj = cc.shape[1]
+
+    lc0 = grid.get((level + 1, (2 * (i - 1), 2 * j + 0)), None)
+    lc1 = grid.get((level + 1, (2 * (i - 1), 2 * j + 1)), None)
+
+    if lc0 is not None:
+        cc[0, : nj // 2] = 0.5 * (lc0[-1, 0:-1:2] + lc0[-1, 1::2])
+    if lc1 is not None:
+        cc[0, nj // 2 :] = 0.5 * (lc1[-1, 0:-1:2] + lc1[-1, 1::2])
+
+
+def correct_flux_cr(index, grid):
+    level, (i, j) = index
+    cc = grid[index]
+    ni = cc.shape[0]
+
+    cr0 = grid.get((level + 1, (2 * i + 0), 2 * (j + 1)), None)
+    cr1 = grid.get((level + 1, (2 * i + 1), 2 * (j + 1)), None)
+
+    if cr0 is not None:
+        cc[: ni // 2, -1] = 0.5 * (cr0[0:-1:2, 0] + cr0[1::2, 0])
+    if cr1 is not None:
+        cc[ni // 2 :, -1] = 0.5 * (cr1[0:-1:2, 0] + cr1[1::2, 0])
+
+
+def correct_flux_cl(index, grid):
+    level, (i, j) = index
+    cc = grid[index]
+    ni = cc.shape[0]
+
+    cl0 = grid.get((level + 1, (2 * i + 0, 2 * (j - 1))), None)
+    cl1 = grid.get((level + 1, (2 * i + 1, 2 * (j - 1))), None)
+
+    if cl0 is not None:
+        cc[: ni // 2, 0] = 0.5 * (cl0[0:-1:2, -1] + cl0[1::2, -1])
+    if cl1 is not None:
+        cc[ni // 2 :, 0] = 0.5 * (cl1[0:-1:2, -1] + cl1[1::2, -1])
+
+
 if __name__ == "__main__":
     DEBUG_MODE = True
 
+    case = 2
     nz = 16
     grid = dict()
 
-    grid[(1, (0, 1))] = cell_centers_2d(1, (0, 1), nz, nz)
-    grid[(2, (2, 3))] = cell_centers_2d(2, (2, 3), nz, nz)
-    grid[(2, (0, 1))] = cell_centers_2d(2, (0, 1), nz, nz)
+    if case == 0:
+        grid[(1, (0, 1))] = cell_centers_2d(1, (0, 1), nz, nz)
+        grid[(2, (2, 3))] = cell_centers_2d(2, (2, 3), nz, nz)
+        grid[(2, (0, 1))] = cell_centers_2d(2, (0, 1), nz, nz)
+
+    if case == 1:
+        grid[(1, (1, 1))] = cell_centers_2d(1, (1, 1), nz, nz)
+        grid[(2, (2, 1))] = cell_centers_2d(2, (2, 1), nz, nz)
+        grid[(2, (1, 2))] = cell_centers_2d(2, (1, 2), nz, nz)
+
+    if case == 2:
+        grid[(1, (1, 0))] = cell_centers_2d(1, (1, 0), nz, nz)
+        grid[(2, (3, 2))] = cell_centers_2d(2, (3, 2), nz, nz)
+        grid[(2, (1, 0))] = cell_centers_2d(2, (1, 0), nz, nz)
+
     z_arrays = {key: (exp(-(x**2 + y**2) / 0.05)) for key, (x, y) in grid.items()}
 
     for z in z_arrays.values():
@@ -207,8 +276,17 @@ if __name__ == "__main__":
         z[:, :+2] = 0.0
         z[:, -2:] = 0.0
 
-    fill_guard_lc((2, (2, 3)), z_arrays)
-    fill_guard_cr((2, (0, 1)), z_arrays)
+    if case == 0:
+        fill_guard_lc((2, (2, 3)), z_arrays)
+        fill_guard_cr((2, (0, 1)), z_arrays)
+
+    if case == 1:
+        fill_guard_rc((2, (1, 2)), z_arrays)
+        fill_guard_cr((2, (2, 1)), z_arrays)
+
+    if case == 2:
+        fill_guard_cl((2, (3, 2)), z_arrays)
+        fill_guard_rc((2, (1, 0)), z_arrays)
 
     vmin = max(z[2:-2, 2:-2].min() for z in z_arrays.values())
     vmax = max(z[2:-2, 2:-2].max() for z in z_arrays.values())
