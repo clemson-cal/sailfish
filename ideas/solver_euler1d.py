f23dc93b193957217d3bbeea666ebbee6a00674b
Jonathan Zrake <jonathan.zrake@gmail.com>
2022-11-23 14:33:31 -0500

    Add option to do flux-per-zone or flux-per-face

--- ideas/solver_euler1d.py
+++ ideas/solver_euler1d.py
@@ -5,10 +5,12 @@ from new_kernels import kernel, perf_time_sequence, configure_kernel_module
 from lib_euler import prim_to_cons, cons_to_prim, riemann_hlle
 
 
-@kernel(
-    device_funcs=[cons_to_prim],
-    define_macros=dict(DIM=1),
-    code=R"""
+@kernel(device_funcs=[cons_to_prim], define_macros=dict(DIM=1))
+def cons_to_prim_array(u: NDArray[float], p: NDArray[float], ni: int = None):
+    R"""
+    //
+    // Convert an array of conserved data to an array of primitive data
+    //
     KERNEL void cons_to_prim_array(double *u, double *p, int ni)
     {
         FOR_EACH_1D(ni)
@@ -16,19 +18,16 @@ from lib_euler import prim_to_cons, cons_to_prim, riemann_hlle
             cons_to_prim(&u[NCONS * i], &p[NCONS * i]);
         }
     }
-    """,
-)
-def cons_to_prim_array(u: NDArray[float], p: NDArray[float], ni: int = None):
-    """
-    Convert an array of conserved data to an array of primitive data
     """
     return u.size // 3, (u, p, u.size // 3)
 
 
-@kernel(
-    device_funcs=[prim_to_cons],
-    define_macros=dict(DIM=1),
-    code=R"""
+@kernel(device_funcs=[prim_to_cons], define_macros=dict(DIM=1))
+def prim_to_cons_array(p: NDArray[float], u: NDArray[float], ni: int = None):
+    R"""
+    //
+    // Convert an array of primitive data to an array of conserved data
+    //
     KERNEL void prim_to_cons_array(double *p, double *u, int ni)
     {
         FOR_EACH_1D(ni)
@@ -36,23 +35,55 @@ def cons_to_prim_array(u: NDArray[float], p: NDArray[float], ni: int = None):
             prim_to_cons(&p[NCONS * i], &u[NCONS * i]);
         }
     }
-    """,
-)
-def prim_to_cons_array(p: NDArray[float], u: NDArray[float], ni: int = None):
-    """
-    Convert an array of primitive data to an array of conserved data
     """
     return p.size // 3, (p, u, p.size // 3)
 
 
+@kernel()
+def conservative_update(
+    u: NDArray[float],
+    f: NDArray[float],
+    dt: float,
+    dx: float,
+    ni: int = None,
+):
+    R"""
+    //
+    // Conservative difference an array of fluxes to update an array of conserved
+    // densities.
+    //
+    KERNEL void conservative_update(
+        double *u,
+        double *f,
+        double dt,
+        double dx,
+        int ni)
+    {
+        FOR_RANGE_1D(1, ni - 1)
+        {
+            double *uc = &u[3 * i];
+            double *fm = &f[3 * i];
+            double *fp = &f[3 * (i + 1)];
+            for (int q = 0; q < 3; ++q)
+            {
+                uc[q] -= (fp[q] - fm[q]) * dt / dx;
+            }
+        }
+    }
+    """
+    return u.size // 3, (u, f, dt, dx, u.size // 3)
+
+
 @kernel(
-    device_funcs=[
-        prim_to_cons,
-        cons_to_prim,
-        riemann_hlle,
-    ],
-    define_macros=dict(DIM=1),
-    code=R"""
+    device_funcs=[prim_to_cons, cons_to_prim, riemann_hlle], define_macros=dict(DIM=1)
+)
+def update_prim_rk1_pcm(p: NDArray[float], dt: float, dx: float, ni: int = None):
+    R"""
+    //
+    // A single-step first-order update using flux-per-zone.
+    //
+    // The first and final elements of the primitive array are not modified.
+    //
     KERNEL void update_prim_rk1_pcm(double *p, double dt, double dx, int ni)
     {
         double uc[NCONS];
@@ -76,21 +107,18 @@ def prim_to_cons_array(p: NDArray[float], u: NDArray[float], ni: int = None):
             cons_to_prim(uc, pc);
         }
     }
-    """,
-)
-def update_prim_rk1_pcm(p: NDArray[float], dt: float, dx: float, ni: int = None):
-    """
-    A single-step first-order update using flux-per-zone.
-
-    The first and final elements of the primitive array are not modified.
     """
     return p.shape[0], (p, dt, dx, p.shape[0])
 
 
-@kernel(
-    device_funcs=[riemann_hlle],
-    define_macros=dict(DIM=1),
-    code=R"""
+@kernel(device_funcs=[riemann_hlle], define_macros=dict(DIM=1))
+def compute_godunov_fluxes_pcm(p: NDArray[float], f: NDArray[float], ni: int = None):
+    R"""
+    //
+    // Compute an array of Godunov fluxes using HLLE Riemann solver.
+    //
+    // The first and final elements of the flux array are not modified.
+    //
     KERNEL void compute_godunov_fluxes_pcm(double *p, double *f, int ni)
     {
         FOR_RANGE_1D(0, ni - 1)
@@ -101,41 +129,36 @@ def update_prim_rk1_pcm(p: NDArray[float], dt: float, dx: float, ni: int = None)
             riemann_hlle(pc, pr, fp, 1);
         }
     }
-    """,
-)
-def compute_godunov_fluxes_pcm(p: NDArray[float], f: NDArray[float], ni: int = None):
-    """
-    Compute an array of Godunov fluxes using HLLE Riemann solver.
-
-    The first and final elements of the flux array are not modified.
     """
     return p.shape[0], (p, f, p.shape[0])
 
 
-class Scratch:
-    def __init__(self, ni, xp):
-        self.ni = ni
-        self.u = xp.zeros([ni, 3])
-        self.f = xp.zeros([ni, 3])
-
-
-def update_prim(p, dt, dx, xp, scratch):
+def update_prim(
+    p,
+    dt,
+    dx,
+    strategy="flux_per_zone",
+    xp=None,
+):
     """
-    Drives a first-order, flux-per-face update of a primitive array
+    Drives a first-order update of a primitive array
     """
-    ni = p.shape[0]
 
-    if scratch is None or scratch.ni != ni:
-        scratch = Scratch(ni, xp)
+    if strategy == "flux_per_face":
+        f = xp.empty_like(p)
+        u = xp.empty_like(p)
 
-    f = scratch.f
-    u = scratch.u
-    prim_to_cons_array(p, u)
-    compute_godunov_fluxes_pcm(p, f)
-    u[1:-1] -= xp.diff(f[1:], axis=0) * (dt / dx)
-    cons_to_prim_array(u, p)
+        prim_to_cons_array(p, u)
+        compute_godunov_fluxes_pcm(p, f)
+        conservative_update(u, f, dt, dx)
+        cons_to_prim_array(u, p)
+        return
 
-    return scratch
+    if strategy == "flux_per_zone":
+        update_prim_rk1_pcm(p, dt, dx)
+        return
+
+    raise ValueError(f"unknown strategy {strategy}")
 
 
 def cell_centers_1d(ni):
@@ -159,6 +182,18 @@ def linear_shocktube(x):
     p[l, :] = [1.0, 0.0, 1.000]
     p[r, :] = [0.1, 0.0, 0.125]
     return p
+
+
+def numpy_or_cupy(mode):
+    if mode == "gpu":
+        import cupy
+
+        return cupy, lambda a: a.get()
+
+    if mode == "cpu":
+        import numpy
+
+        return numpy, lambda a: a
 
 
 @logger.catch
@@ -219,6 +254,11 @@ def main():
         action="store_true",
         help="show a plot after the run",
     )
+    parser.add_argument(
+        "--strategy",
+        choices=["flux_per_zone", "flux_per_face"],
+        default="flux_per_face",
+    )
     add_logging_arguments(parser)
     args = parser.parse_args()
 
@@ -226,6 +266,7 @@ def main():
     term = terminal(logger)
 
     configure_kernel_module(default_exec_mode=args.exec_mode)
+    xp, to_host = numpy_or_cupy(args.exec_mode)
 
     nz = args.resolution or 100000
     dx = 1.0 / nz
@@ -235,23 +276,13 @@ def main():
     t = 0.0
     n = 0
 
-    if args.exec_mode == "gpu":
-        import cupy as xp
-
-        to_host = lambda a: a.get()
-    else:
-        import numpy as xp
-
-        to_host = lambda a: a
-
     p = xp.array(p)
     perf_timer = perf_time_sequence(mode=args.exec_mode)
 
     logger.info("start simulation")
-    scratch = None
 
     while t < 0.1:
-        scratch = update_prim(p, dt, dx, xp, scratch)
+        update_prim(p, dt, dx, args.strategy, xp)
         t += dt
         n += 1
 
@@ -260,9 +291,6 @@ def main():
             term(iteration_msg(iter=n, time=t, zps=zps))
 
     p = to_host(p)
-    from numpy import save
-
-    save("prim.npy", p)
 
     if args.plot:
         from matplotlib import pyplot as plt
