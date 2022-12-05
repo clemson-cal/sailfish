f39ffeae51f44b522219bf6bc8dbd579885b1709
Jonathan Zrake <jonathan.zrake@gmail.com>
2022-12-05 13:18:32 -0500

    Factor the 1d transposed data solver a little
    Next the other solver variations need to brought back. They
    will probably not be written as one-class-per-variation,
    but more likely with the variations written as possibly
    unused kernel functions in a large kernel class.

--- ideas/solver.py
+++ ideas/solver.py
@@ -21,27 +21,6 @@ def numpy_or_cupy(exec_mode):
         return numpy
 
 
-def linear_shocktube(x):
-    """
-    A linear shocktube setup
-    """
-
-    l = x < 0.5
-    r = logical_not(l)
-    p = zeros(x.shape + (3,))
-    p[l, :] = [1.0, 0.0, 1.000]
-    p[r, :] = [0.1, 0.0, 0.125]
-    return p
-
-
-def cell_centers_1d(ni):
-    from numpy import linspace
-
-    xv = linspace(0.0, 1.0, ni + 1)
-    xc = 0.5 * (xv[1:] + xv[:-1])
-    return xc
-
-
 @device
 def plm_minmod(yl: float, yc: float, yr: float, plm_theta: float):
     R"""
@@ -66,42 +45,28 @@ def plm_minmod(yl: float, yc: float, yr: float, plm_theta: float):
 
 @kernel_class
 class PrimitiveToConserved:
-    def __init__(self, dim=1, lds=False):
+    def __init__(self, dim=1):
         self.dim = dim
-        self.lds = lds
-
-    @classmethod
-    def init(cls):
-        cls.dim1_lds0 = PrimitiveToConserved(dim=1, lds=False)
-        cls.dim1_lds1 = PrimitiveToConserved(dim=1, lds=True)
-        cls.dim2_lds0 = PrimitiveToConserved(dim=2, lds=False)
-        cls.dim2_lds1 = PrimitiveToConserved(dim=2, lds=True)
-
-    @classmethod
-    def get(cls, dim, lds):
-        if dim == 1 and not lds:
-            return cls.dim1_lds0
-        if dim == 1 and lds:
-            return cls.dim1_lds1
-        if dim == 2 and not lds:
-            return cls.dim2_lds0
-        if dim == 2 and lds:
-            return cls.dim2_lds1
 
     @property
     def define_macros(self):
-        return dict(DIM=self.dim, LDS_MODE=int(self.lds))
+        return dict(DIM=self.dim)
 
     @property
     def device_funcs(self):
         return [prim_to_cons, cons_to_prim]
 
     @kernel
-    def cons_to_prim_array(self, u: NDArray[float], p: NDArray[float], ni: int = None):
+    def cons_to_prim_fields_last(
+        self,
+        u: NDArray[float],
+        p: NDArray[float],
+        ni: int = None,
+    ):
         R"""
-        KERNEL void cons_to_prim_array(double *p, double *u, int ni)
+        KERNEL void cons_to_prim_fields_last(double *u, double *p, int ni)
         {
-            FOR_RANGE_1D(0, ni)
+            FOR_EACH_1D(ni)
             {
                 cons_to_prim(&u[NCONS * i], &p[NCONS * i]);
             }
@@ -109,73 +74,128 @@ class PrimitiveToConserved:
         """
         nq = self.dim + 2
 
-        if u.shape[-1] != nq or u.shape != u.shape:
+        if u.shape[-1] != nq or u.shape != p.shape:
             raise ValueError("array has wrong number of fields")
 
         return u.size // nq, (u, p, u.size // nq)
 
     @kernel
-    def prim_to_cons_array(self, p: NDArray[float], u: NDArray[float], ni: int = None):
+    def prim_to_cons_fields_last(
+        self,
+        p: NDArray[float],
+        u: NDArray[float],
+        ni: int = None,
+    ):
         R"""
-        KERNEL void prim_to_cons_array(double *p, double *u, int ni)
+        KERNEL void prim_to_cons_fields_last(double *p, double *u, int ni)
         {
-            #if LDS_MODE == 0 || defined(CPU_MODE)
-
-            FOR_RANGE_1D(0, ni)
+            FOR_EACH_1D(ni)
             {
                 prim_to_cons(&p[NCONS * i], &u[NCONS * i]);
             }
-
-            #elif LDS_MODE == 1
-
-            if (blockIdx.x * blockDim.x + threadIdx.x >= ni)
-            {
-                return;
-            }
-            __shared__ double p_lds[64 * NCONS]; // this assumes the thread block size is 64!
-
-            double *p_blk = &p[blockIdx.x * blockDim.x * NCONS];
-            double *u_blk = &u[blockIdx.x * blockDim.x * NCONS];
-
-            // ----------------------------------------------------------------
-            // Fetch from global memory to LDS, coalesced.
-            //
-            // For example if there were 4 threads in the thread block:
-            //
-            // p:         [[x x x] [x x x] [x x x] [x x x]]
-            // thread:      0 1 2   3 0 1   2 3 0   1 2 3
-            // ----------------------------------------------------------------
-            for (int q = 0; q < NCONS; ++q)
-            {
-                int n = threadIdx.x + blockDim.x * q;
-                p_lds[n] = p_blk[n];
-            }
-            __syncthreads();
-
-            // ----------------------------------------------------------------
-            // Perform one cons -> prim per thread. It's done in-place.
-            // ----------------------------------------------------------------
-            double *u_lds = p_lds;
-            prim_to_cons(&p_lds[threadIdx.x * NCONS], &u_lds[threadIdx.x * NCONS]);
-            __syncthreads();
-
-            // ----------------------------------------------------------------
-            // Write the result back to global memory, coalesced.
-            // ----------------------------------------------------------------
-            for (int q = 0; q < NCONS; ++q)
-            {
-                int n = threadIdx.x + blockDim.x * q;
-                u_blk[n] = u_lds[n];
-            }
-            #else
-            #error("LDS_MODE must be [0|1]")
-            #endif
         }
         """
-
         nq = self.dim + 2
 
         if p.shape[-1] != nq or p.shape != u.shape:
+            raise ValueError("array has wrong number of fields")
+
+        return p.size // nq, (p, u, p.size // nq)
+
+    @kernel
+    def cons_to_prim_fields_first(
+        self,
+        u: NDArray[float],
+        p: NDArray[float],
+        ni: int = None,
+    ):
+        R"""
+        KERNEL void cons_to_prim_fields_first(double *u, double *p, int ni)
+        {
+            #if defined(CPU_MODE)
+            #define blockIdx_x 0
+            #define blockDim_x ni
+            #define threadIdx_x i
+            #elif defined(GPU_MODE)
+            #define blockIdx_x blockIdx.x
+            #define blockDim_x blockDim.x
+            #define threadIdx_x threadIdx.x
+            #endif
+
+            FOR_EACH_1D(ni)
+            {
+                double *u_blk[NCONS];
+                double *p_blk[NCONS];
+                double u_reg[NCONS];
+                double p_reg[NCONS];
+
+                for (int q = 0; q < NCONS; ++q)
+                {
+                    p_blk[q] = &p[q * ni + blockIdx_x * blockDim_x];
+                    u_blk[q] = &u[q * ni + blockIdx_x * blockDim_x];
+                    u_reg[q] = u_blk[q][threadIdx_x];
+                }
+                cons_to_prim(u_reg, p_reg);
+
+                for (int q = 0; q < NCONS; ++q)
+                {
+                    p_blk[q][threadIdx_x] = p_reg[q];
+                }
+            }
+        }
+        """
+        nq = self.dim + 2
+
+        if u.shape[0] != nq or u.shape != p.shape:
+            raise ValueError("array has wrong number of fields")
+
+        return u.size // nq, (u, p, u.size // nq)
+
+    @kernel
+    def prim_to_cons_fields_first(
+        self,
+        p: NDArray[float],
+        u: NDArray[float],
+        ni: int = None,
+    ):
+        R"""
+        KERNEL void prim_to_cons_fields_first(double *p, double *u, int ni)
+        {
+            #if defined(CPU_MODE)
+            #define blockIdx_x 0
+            #define blockDim_x ni
+            #define threadIdx_x i
+            #elif defined(GPU_MODE)
+            #define blockIdx_x blockIdx.x
+            #define blockDim_x blockDim.x
+            #define threadIdx_x threadIdx.x
+            #endif
+
+            FOR_EACH_1D(ni)
+            {
+                double *u_blk[NCONS];
+                double *p_blk[NCONS];
+                double u_reg[NCONS];
+                double p_reg[NCONS];
+
+                for (int q = 0; q < NCONS; ++q)
+                {
+                    u_blk[q] = &u[q * ni + blockIdx_x * blockDim_x];
+                    p_blk[q] = &p[q * ni + blockIdx_x * blockDim_x];
+                    p_reg[q] = p_blk[q][threadIdx_x];
+                }
+                prim_to_cons(p_reg, u_reg);
+
+                for (int q = 0; q < NCONS; ++q)
+                {
+                    u_blk[q][threadIdx_x] = u_reg[q];
+                }
+            }
+        }
+        """
+        nq = self.dim + 2
+
+        if p.shape[0] != nq or p.shape != u.shape:
             raise ValueError("array has wrong number of fields")
 
         return p.size // nq, (p, u, p.size // nq)
@@ -260,7 +280,7 @@ class FluxPerZoneTransposedSolver:
         return urd.shape[1], (urd, uwr, dt, dx, urd.shape[1])
 
 
-class State1dTranspose:
+class State:
     def __init__(self, n, t, u, p2c):
         self._n = n
         self._t = t
@@ -277,20 +297,39 @@ class State1dTranspose:
 
     @property
     def primitive(self):
-        u = self._u.T.copy()
+        u = self._u
         p = u.copy()
-        self._p2c.cons_to_prim_array(u, p)
+        self._p2c.cons_to_prim_fields_first(u, p)
         try:
-            return p.get()
+            return p.T.get()
         except AttributeError:
-            return p
+            return p.T
 
     @property
     def total_zones(self):
         return self._u.shape[1]
 
 
-def simulation1d_transpose(hardware: str, resolution: int) -> State1dTranspose:
+def simulation(hardware: str, resolution: int) -> State:
+    def linear_shocktube(x):
+        """
+        A linear shocktube setup
+        """
+
+        l = x < 0.5
+        r = logical_not(l)
+        p = zeros(x.shape + (3,))
+        p[l, :] = [1.0, 0.0, 1.000]
+        p[r, :] = [0.1, 0.0, 0.125]
+        return p
+
+    def cell_centers_1d(ni):
+        from numpy import linspace
+
+        xv = linspace(0.0, 1.0, ni + 1)
+        xc = 0.5 * (xv[1:] + xv[:-1])
+        return xc
+
     xp = numpy_or_cupy(hardware)
     nz = resolution
     dx = 1.0 / nz
@@ -304,19 +343,19 @@ def simulation1d_transpose(hardware: str, resolution: int) -> State1dTranspose:
 
     solver = FluxPerZoneTransposedSolver()
     p2c = PrimitiveToConserved(dim=1)
-    p2c.prim_to_cons_array(p, u)
+    p2c.prim_to_cons_fields_last(p, u)
     u1 = xp.ascontiguousarray(u.T)
     u2 = u1.copy()
 
-    yield State1dTranspose(n, t, u1, p2c)
+    yield State(n, t, u1, p2c)
 
     while True:
         solver.update_cons(u1, u2, dt, dx)
         u1, u2 = u2, u1
         t += dt
         n += 1
-        yield State1dTranspose(n, t, u1, p2c)
+        yield State(n, t, u1, p2c)
 
 
 def make_solver(app: Sailfish):
-    return simulation1d_transpose(app.hardware, app.domain.num_zones[0])
+    return simulation(app.hardware, app.domain.num_zones[0])
